7057a663687540bcde05a0880a0fc1b5f3163376
Marcus Wiseman <md.wiseman@hotmail.com>
2022-03-27 15:26:08 +0200

    Sanity query

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,24 +5,45 @@
     import ListItem from '$lib/components/listItem.svelte';
 
     const query = `
-            *[_type == 'game']
+            *[_type == 'season']
             {
                 _id,
-                date, 
-                description, 
-                homeTeam->{name, logo}, 
-                awayTeam->{name, logo}, 
-                homeTeamScore, 
-                awayTeamScore,
-                image,
+                year,
+                "schedules": schedules[]{
+                    _id,
+                    league->{name},
+                    "games": games[]{
+                        awayTeam->{name, logo},
+                        homeTeam->{name, logo},
+                        awayTeamScore,
+                        homeTeamScore,
+                        date,
+                    },
+                },
             }
         `;
-    let games = [];
-    (async () => (games = await sanity.fetch(query)))();
+    let seasons = [];
+    (async () => (seasons = await sanity.fetch(query)))();
 </script>
 
-<List>
-    {#each games as game}
-        <ListItem {game} clickable={true} />
+<div>
+    {#each seasons as { _id, year, schedules } (_id)}
+        <div class="mt-6">
+            <h3 class="text-2xl font-semibold mb-2">{year}</h3>
+
+            {#if schedules?.length}
+                {#each schedules as { _id, league, games }}
+                    <li>{league.name}</li>
+
+                    {#if games?.length}
+                        <List>
+                            {#each games as game}
+                                <ListItem {game} clickable={true} />
+                            {/each}
+                        </List>
+                    {/if}
+                {/each}
+            {/if}
+        </div>
     {/each}
-</List>
+</div>
