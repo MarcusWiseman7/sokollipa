fd1d5de05b25d96f8a1cbf5b6c494c1e7c99bdfa
Marcus Wiseman <md.wiseman@hotmail.com>
2022-07-17 14:05:46 +0200

    Player checks

--- src/lib/components/player.svelte
+++ src/lib/components/player.svelte
@@ -8,30 +8,40 @@
     <div class="grid gap-5 grid-cols-1 lg:grid-cols-2 w-full lg:h-[440px] shadow-lg rounded-lg">
         <div class="relative py-[20px]">
             <h3 class="text-4xl font-semibold text-center mb-6 lg:hidden">{player.name}</h3>
-            <img
-                src={urlFor(player.profilePhoto).height(400).format('webp').url()}
-                srcset={urlFor(player.profilePhoto).height(800).format('webp').url()}
-                alt={player.name}
-                class="max-w-full max-h-[400px] w-auto"
-                on:contextmenu|preventDefault
-                on:dragstart|preventDefault
-            />
-            <div class="absolute bottom-4 right-4 text-8xl font-['Varsity']">{player.number}</div>
+            {#if player.profilePhoto}
+                <img
+                    src={urlFor(player.profilePhoto).height(400).format('webp').url()}
+                    srcset={urlFor(player.profilePhoto).height(800).format('webp').url()}
+                    alt={player.name}
+                    class="max-w-full max-h-[400px] w-auto"
+                    on:contextmenu|preventDefault
+                    on:dragstart|preventDefault
+                />
+            {/if}
+            {#if player.number}
+                <div class="absolute bottom-4 right-4 text-8xl font-['Varsity']">{player.number}</div>
+            {/if}
         </div>
         <div class="py-[20px] px-7 lg:px-0">
-            <img
-                src={urlFor(player.avatar).height(80).width(80).format('webp').url()}
-                srcset={urlFor(player.avatar).height(160).width(80).format('webp').url()}
-                alt={player.name}
-                height="80"
-                width="80"
-                class="hidden lg:block max-h-[80px] max-w-[80px] object-contain rounded-full"
-                on:contextmenu|preventDefault
-                on:dragstart|preventDefault
-            />
+            {#if player.avatar}
+                <img
+                    src={urlFor(player.avatar).height(80).width(80).format('webp').url()}
+                    srcset={urlFor(player.avatar).height(160).width(80).format('webp').url()}
+                    alt={player.name}
+                    height="80"
+                    width="80"
+                    class="hidden lg:block max-h-[80px] max-w-[80px] object-contain rounded-full"
+                    on:contextmenu|preventDefault
+                    on:dragstart|preventDefault
+                />
+            {/if}
             <h3 class="text-4xl font-semibold hidden lg:block">{player.name}</h3>
-            <h4 class="text-3xl font-semibold text-center lg:text-left">{player.position}</h4>
-            <h5 class="text-2xl text-justify lg:text-left">{player.info}</h5>
+            {#if player.position}
+                <h4 class="text-3xl font-semibold text-center lg:text-left">{player.position}</h4>
+            {/if}
+            {#if player.info}
+                <h5 class="text-2xl text-justify lg:text-left">{player.info}</h5>
+            {/if}
         </div>
     </div>
 {/if}
