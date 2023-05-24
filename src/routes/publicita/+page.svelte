e994aae69a79aa9d50c835d345a70bd0bd09d440
Marcus Wiseman <md.wiseman@hotmail.com>
2023-05-24 17:38:32 +0200

    New publicita image

--- src/routes/publicita/+page.svelte
+++ src/routes/publicita/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
     import pic from '$lib/assets/1.webp';
+    import pic2 from '$lib/assets/2.webp';
 </script>
 
 <div class="page">
     <div class="pic">
         <img src={pic} alt="EU growth program" />
+        <img src={pic2} alt="EU growth program information" />
     </div>
 </div>
 
